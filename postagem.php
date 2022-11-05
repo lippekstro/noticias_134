41bfbd5cb62902c9c9be9286fffca866573361fa
PauloMatosPM <paulopereirama@gmail.com>
2022-11-04 22:13:08 -0300

    atualização postagem

--- postagem.php
+++ postagem.php
@@ -1,10 +1,23 @@
 <?php
 
 class Postagem{
+    public $id_post;
     public $titulo;
     public $conteudo;
     public $imagem;
     public $id_autor;
     public $id_categoria;
     public $date;
+}
+
+public function inserir(){
+    $query = "INSERT INTO postagem (titulo, conteudo, imagem, id_autor, id_categoria) VALUES (:titulo, :conteudo, :imagem, :id_autor, :id_categoria)"; 
+    $conexao = Conexao::conectar();
+    $stmt = $conexao->prepare($query);
+    $stmt->bindValue(':titulo', $this->titulo);
+    $stmt->bindValue(':conteudo', $this->conteudo);
+    $stmt->bindValue(':imagem', $this->imagem);
+    $stmt->bindValue(':id_autor', $this->id_autor);
+    $stmt->bindValue(':id_categoria', $this->id_categoria);
+    $stmt->execute();
 }
--- usuario.php
+++ usuario.php
@@ -8,3 +8,14 @@ class Usuario{
     public $nivel_acesso;
 
 }
+
+public function deletar(){
+    $query = "DELETE FROM usuario WHERE id_usuario=:id_usuario";
+    $conexao = Conexao::conectar();
+    $stmt = $conexao->prepare($query);
+    $stmt->bindValue(":id_usuario", $this->id_usuario);
+    $stmt->execute();
+}
+public function editar(){
+    $query = "UPDATE usuario SET nome = :nome, email = :email, senha = :senha WHERE id_usuario = :id_usuario";
+}
