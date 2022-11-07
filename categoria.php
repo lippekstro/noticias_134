aff6a122381eaa5f9151a6f54fbdb8157f5e2937
MilsonCarlos <milsonaluno@gmail.com>
2022-11-07 17:45:17 -0300

    Editado

--- categoria.php
+++ categoria.php
@@ -5,4 +5,15 @@ class Categoria {
     public $nome_categoria;
     public $id_categoria;
 
+
+    public static function listar()
+    {
+        $query = "select nome_categoria";
+
+        $conexao = Conexao::conectar();
+        $resultado = $conexao->query($query);
+        $lista = $resultado->fetchAll();
+        return $lista;
+    }
+
 }
