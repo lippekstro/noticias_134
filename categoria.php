dfa4e8437df366bae8c60bcf899443a08342b69c
lucasjabota <lucas.araujo.costa13@gmail.com>
2022-11-07 17:53:26 -0300

    criei função editar

--- categoria.php
+++ categoria.php
@@ -5,4 +5,13 @@ class Categoria {
     public $nome_categoria;
     public $id_categoria;
 
+    public function editar() {
+        $query = "UPDATE categoria SET nome_categoria = :nome_categoria WHERE id_categoria = :id_categoria";
+        $conexao = Conexao::conectar();
+        $stmt = $conexao->prepare($query);
+        $stmt->bindValue(":nome_categoria", $this->nome_categoria);
+
+        $stmt->execute();
+    }
+
 }
