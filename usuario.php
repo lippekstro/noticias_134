aeefe6fd94dfcf3fed0e8e6373804399f80fd899
PauloMatosPM <paulopereirama@gmail.com>
2022-11-08 14:28:47 -0300

    editar user e editar pelo administrador

--- usuario.php
+++ usuario.php
@@ -16,6 +16,21 @@ public function deletar(){
     $stmt->bindValue(":id_usuario", $this->id_usuario);
     $stmt->execute();
 }
-public function editar(){
+public function editar_usuario(){
     $query = "UPDATE usuario SET nome = :nome, email = :email, senha = :senha WHERE id_usuario = :id_usuario";
+    $conexao = Conexao::conectar();
+    $stmt = $conexao->prepare($query);
+    $stmt->bindValue(":nome", $this->nome);
+    $stmt->bindValue(":email", $this->email);
+    $stmt->bindValue(":senha", $this->senha);
+}
+
+public function editar_admin(){
+    $query = "UPDATE usuario SET nome = :nome, email = :email, senha = :senha, nivel_acesso = :nivel_acesso WHERE id_usuario = :id_usuario";
+    $conexao = Conexao::conectar();
+    $stmt = $conexao->prepare($query);
+    $stmt->bindValue(":nome", $this->nome);
+    $stmt->bindValue(":email", $this->email);
+    $stmt->bindValue(":senha", $this->senha);
+    $stmt->bindValue(":nivel_acesso", $this->nivel_acesso);
 }
