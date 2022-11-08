056de9d26e74aef54b4dc6478ac985d12d432573
italorc123 <italorabelocarvalho39@gmail.com>
2022-11-08 15:00:11 -0300

    fiz a funçao deletar postagem

--- postagem.php
+++ postagem.php
@@ -2,9 +2,23 @@
 
 class Postagem{
     public $titulo;
+    public $id_postagem
     public $conteudo;
     public $imagem;
     public $id_autor;
     public $id_categoria;
     public $date;
+}
+
+public function deletar (){
+        $query = "DELETE FROM Postagem WHERE id_postagem=:id_postagem";
+        //deleta pelo id
+        $conexao = Conexao::conectar();
+        // cria conexao
+        $stmt = $conexao->prepare($query);
+        // prepara a query
+        $stmt->bindValue("id_postagem", $this->id_postagem);
+        // vincula o valor
+        $stmt->execute();
+        // executa
 }
