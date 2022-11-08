0d9ce74dc158c9c80c9046725226d69ddb920302
PedroMartinszx <pedro1898lemos1898@hotmail.com>
2022-11-07 22:03:11 -0300

    criei e deletei

--- categoria.php
+++ categoria.php
@@ -6,3 +6,29 @@ class Categoria {
     public $id_categoria;
 
 }
+   
+public function DELETAR(){
+    $query = "DELETE FROM categoria WHERE id_categoria = :id_categoria";
+    $conexao = Conexao::conectar();
+    $stmt = $conexao->prepare($query);
+    $stmt->binValue(':id_categoria' , $this->id_pessoa);
+    $stmt->execute();
+
+}
+
+
+
+
+
+public function INSERIR(){
+    $query = "INSERT INTO categoria (nome_categoria, id_categoria)
+    VALUES (:nome, :id)";
+    $conexao = Conexao::conectar();
+    $stmt = $conexao->prepare($query);
+    $stmt->bindValue(':nome', $this->nome_categoria);
+    $stmt->bindValue(':idade', $this->id_categoria);
+    $stmt->execute();
+
+
+}
+   
