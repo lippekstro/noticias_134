07d21cf2edc28cca54b026ce35a11b77542ba886
weunji <eunjivicente@gmail.com>
2022-11-09 09:53:38 +0900

    Adicionei a pagina sobre e exibicao do post

---
+++ post_exibicao.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=>, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div style="display: flex;" id="centro">
+        <div class="post">
+            <div style="width: 60vw; margin: auto; text-align: justify;">
+                <h3 style="margin: 1.5 0; font-size:36px; text-align: justify;">Apink: "A maior recompensa é poder receber tanto amor "</h1>
+                <p>Apink é um grupo feminino sul-coreano formado pela Plan A Entertainment em 2011. É composto por cinco integrantes: Chorong, Bomi, Eunji, Namjoo e Hayoung. Sua formação original incluía Hong Yoo-kyung|YooKyung e Son Naeun|Na-Eun. Elas estrearam em 21 de abril de 2011 </p>
+                <h3 style="color: black; font-size:19px; margin-top: 1cm ;">Por AUTOR</h3>
+                <p style="margin-bottom: 1cm;">09/11/2022 13h07</p>
+                <hr><br>
+            </div>
+            
+            <div style="width: 50vw; margin: auto; padding-top: 1cm;">
+                <img id="img_post" src="https://s2.glbimg.com/Ut37qguDndhnDZgz-SQswTV8sI4=/620x466/top/e.glbimg.com/og/ed/f/original/2022/02/21/apink_teaser_black_ver.jpg" alt="">
+                <p class="text_post" style="text-align: justify; margin: 2cm;">O sexteto, formado por Chorong, Bomi, Eunji, Naeun, Namjoo e Hayoung, é um dos grupos femininos mais antigos ainda ativos e continuam colecionando sucesso atrás de sucesso na carreira.
+                    Para a nova title Dilemma, elas se uniram mais uma vez com o duo de produtores Black Eyed Pilseung, nome por trás de outros hits do grupo como I'm So Sick, de 2018, %%, de 2019, e Dumhdurum, de 2020. Apesar da parceria, elas também participaram da composição de seis das 11 faixas do álbum.
+                    
+                    Em um papo com Quem, elas celebraram o lançamento e a importância de estarem envolvidas na criação de sua música, além de refletirem sobre seus 10 anos de carreira e revelarem o desejo de conhecer o Brasil.
+                    
+                    (Naeun não participou da entrevista e não está promovendo o álbum com as outras integrantes por um conflito de agenda).</p>
+            </div>
+        </div>
+
+    </div>
+</body>
+</html>
