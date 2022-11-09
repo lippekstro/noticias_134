8d8ff2f7a3cc6fb93018004dce4174bf2b2cd7f8
Dnmaster-Dev <estevaothatymartins2325@gmail.com>
2022-11-09 17:38:12 -0300

    fiz o cabecalho

---
+++ cabecalho.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Catamaran:wght@100&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="CSS/style.css">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <header>
+
+
+        <div class="navbar">
+            <div class="container-barra">
+                <div>
+                    <a href="index.php"><img id="container-logo" src="img/logo-removebg-preview.png" alt=""></a>
+                </div>
+
+                <div>
+                    <div class="conteiner-search">
+                        <form id="pesquisa" action="">
+                            <div class="conteiner-form-input-btn">
+                                <input size="200" type="search" name="" id="barra-pesquisa">
+                                <button type="submit"><span class="material-symbols-outlined">search</span></button>
+                            </div>
+                        </form>
+                        <div>
+                            <a href="login.php"><span id="account_circle" class="material-symbols-outlined">account_circle</span></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="container-menu-global">
+                <ul class="menu-global">
+                    <li><a href="index.php">Início</a></li>
+                    <div class="dropdown">
+                        <li>Categoria</li>
+                        <div class="dropdown-content">
+                            <ul id="links-global">
+                                <li><a href="noticia.php?cat=esportes">Esportes</a></li>
+                                <li><a href="noticia.php?cat=tecnologia">Tecnologia</a></li>
+                                <li><a href="noticia.php?cat=economia">Econômia</a></li>
+                                <li><a href="noticia.php?cat=local">Notícias Locais</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <li><a href="sobre.php">Sobre nós</a></li>
+                </ul>
+            </div>
+
+        </div>
+    </header>
+    <main>
--- index.php
+++ index.php
@@ -1,47 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-    <link rel="stylesheet" href="css/style.css">
-    <title>Document</title>
-</head>
-
-<body>
-
-    <div class="navbar container-nav">
-        <div class="container-barra">
-            <div>
-                <img id="container-logo" src="img/logo-removebg-preview.png" alt="">
-            </div>
-
-            <div>
-                <form id="pesquisa" action="">
-                    <input type="search" name="" id="">
-                    <button type="submit"><span class="material-symbols-outlined">search</span></button>
-                </form>
-                <div>
-                    <a href=""><span class="material-symbols-outlined">person</span></a>
-                </div>
-            </div>
-        </div>
-        <div class="container-menu-global">
-            <ul class="menu-global">
-            <li><a href="home.html">Início</a></li>
-            <li><a href="carrinho.html">Categoria</a></li>
-            <li><a href="login.html">Sobre nós</a></li>
-            </ul>
-        </div>
-    </div>
-
-    <h1>ULTIMAS NOTÍCIAS</h1>
+<?php
+require_once "cabecalho.php";
+?>
+  
+    <h1 class="h1">Ultimas Notícias</h1>
 
 
     <div class="flex">
-        <div class="card" style="width:10%">
+        <div class="card" style="width: 13%">
             <img src="img/logo.png" alt="Avatar">
             <div class="container">
                 <h4><b>TÍTULO</b></h4>
@@ -51,7 +16,7 @@
 
 
 
-        <div class="card" style="width:10%">
+        <div class="card" style="width: 13%">
             <img src="img/logo.png" alt="Avatar">
             <div class="container">
                 <h4><b>TÍTULO</b></h4>
@@ -61,7 +26,7 @@
 
 
 
-        <div class="card" style="width:10%">
+        <div class="card" style="width: 13%">
             <img src="img/logo.png" alt="Avatar">
             <div class="container">
                 <h4><b>TÍTULO</b></h4>
@@ -70,7 +35,7 @@
         </div>
 
 
-        <div class="card" style="width:10%">
+        <div class="card" style="width: 13%">
             <img src="img/logo.png" alt="Avatar">
             <div class="container">
                 <h4><b>TÍTULO</b></h4>
@@ -80,8 +45,7 @@
 
 
     </div>
-
-
+    </main>
 </body>
 
 </html>
