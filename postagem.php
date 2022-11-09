d663876fec0be3c5f129187c493ce22e6645a677
lippekstro <fellipecastro13@hotmail.com>
2022-11-09 17:11:27 -0300

    mais ajustes

--- postagem.php
+++ postagem.php
@@ -8,7 +8,7 @@ class Postagem
     public $imagem;
     public $id_autor;
     public $id_categoria;
-    public $data;
+    public $data_pub;
 
     public function inserir()
     {
@@ -27,7 +27,7 @@ class Postagem
 
     public function editar_postagem()
     {
-        $query = "UPDATE postagem SET titulo = :titulo, conteudo = :conteudo, imagem = :imagem, id_autor = :id_autor, id_categoria = :id_categoria, data = :data WHERE id_postagem = :id_postagem";
+        $query = "UPDATE postagem SET titulo = :titulo, conteudo = :conteudo, imagem = :imagem, id_autor = :id_autor, id_categoria = :id_categoria, data_pub = :data_pub WHERE id_postagem = :id_postagem";
         $conexao = Conexao::conectar();
         $stmt = $conexao->prepare($query);
         $stmt->bindValue(':titulo', $this->titulo);
@@ -35,7 +35,7 @@ class Postagem
         $stmt->bindValue(':imagem', $this->imagem);
         $stmt->bindValue(':id_autor', $this->id_autor);
         $stmt->bindValue(':id_categoria', $this->id_categoria);
-        $stmt->bindValue(':data', $this->data);
+        $stmt->bindValue(':data_pub', $this->data_pub);
         $stmt->bindValue(':id_postagem', $this->id_postagem);
 
         $stmt->execute();
@@ -43,7 +43,7 @@ class Postagem
 
     public static function listar()
     {
-        $query = "SELECT titulo, nome_categoria, conteudo, imagem, id_autor, id_categoria, data, id_postagem FROM Postagem ";
+        $query = "SELECT titulo, nome_categoria, conteudo, imagem, id_autor, id_categoria, data_pub, id_postagem FROM Postagem ";
         $conexao = Conexao::conectar();
         $resultado = $conexao->query($query);
         $lista = $resultado->fetchAll();
