5f936800594586d762bc0d4df419edac9a03605b
Geovannehh <geovanepaix19@gmail.com>
2022-11-14 17:21:35 -0300

    rodape criado

---
+++ rodape.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>rodape</title>
+    <link rel="stylesheet" href="css/rodape.css">
+    <h1>HELLO WORLD!</h1>
+</head>
+<body>
+    <h2>Corpo da pagina</h2>
+    <footer>
+        
+        <div class="footer-content">
+          
+            <p class="alinhar">
+                &copy;2022 - TODOS OS DIREITOS RESERVADOS
+            </p>
+            <div class="social-list">
+                <ul>
+                    <li><a href=""><i class="fab fa-facebook-f"></i></a></li>
+                    <li><a href=""><i class="fab fa-instagram"></i></a></li>
+                </ul>
+              
+            </div>
+        </div>
+        
+        <article class="site">
+        <a>
+            <p class="site">www.noticias123.com.br</p>
+        </a>
+        </article>
+
+    </footer>
+    
+</body>
+</html>
