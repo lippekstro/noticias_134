60bd779d1725ceb8e71a4b2fcda946bf7eac6e0c
Tesouro <bryantalison04@gmail.com>
2022-11-14 16:24:41 -0300

    edicao de categorias

---
+++ edicao_categoria.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/edicao_categoria.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Catamaran:wght@100&display=swap" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+    <div id="flex">
+      <div id="nome">
+            <label for="">Nome da Categoria</label>
+            <input type="text" id="categoria" name="categoria" s>
+    </div>
+    <div>
+        <button id="btn" type="submit">Alterar</button>
+    </div>
+    </div>
+</body>
+</html>
