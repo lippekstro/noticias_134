42fe8073e884fe61b364db9e86d8e0e479f48252
Tesouro <bryantalison04@gmail.com>
2022-11-14 15:11:46 -0300

    carrosel de 3 img

---
+++ criar_carrosel.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/carrosel.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="slide">
+        <div class="slides"><img src="https://source.unsplash.com/random?landscape,mountain"/></div>
+        <div class="slides"><img src="https://source.unsplash.com/random?landscape,night"/></div>
+        <div class="slides"><img src="https://source.unsplash.com/random?landscape,city"/></div>
+
+        <button class="btn btn_direita">></button>
+        <button class="btn btn_esquerda"><</button>
+    </div>
+    <script src="js/carrosel.js"></script>
+</body>
+</html>
