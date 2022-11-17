ffdfc18f43d71c8a5a51752f8d82e9ec60f7046e
lippekstro <fellipecastro13@hotmail.com>
2022-11-17 15:58:16 -0300

    ajustes

--- cabecalho.php
+++ cabecalho.php
@@ -1,6 +1,6 @@
 <?php
 require_once 'categoria.php';
-require_once  'conexao.php';
+require_once 'conexao.php';
 
 try {
     $lista_categorias = Categoria::listar();
@@ -8,13 +8,7 @@ try {
     echo $e->getMessage();
 }
 
-
-
-
-
 ?>
-
-
 
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -32,7 +26,7 @@ try {
     <link rel="stylesheet" href="css/gerenc_post.css">
     <link rel="stylesheet" href="css/carrosel.css">
     <link rel="stylesheet" href="css/rodape.css">
-    <link rel="stylesheet" href="css/mystyle.css">
+    
     <link rel="stylesheet" href="css/edicao_categoria.css">
     <title>Document</title>
 </head>
